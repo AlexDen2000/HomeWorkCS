4b599bf9a071e75e16438081ca6bc2331cc9249e
Alex <ADenisov2000@yandex.ru>
2021-03-11 10:53:17 +0500

    first commit

---
+++ HomeWorkCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWorkCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
