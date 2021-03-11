c8afe63536cfc24d6bdaf1a43fdf2e6e516099b6
Alex <ADenisov2000@yandex.ru>
2021-03-11 18:23:26 +0500

    task 1,2,3 done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace HomeWork_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var student = new Students();
+            student.name = "Alexander";
+            student.height = 180;
+            student.age = 18;
+            student.ballHistory = 70;
+            student.ballLanguage = 68;
+            student.ballMatematics = 59;
+            Console.WriteLine("name = " + student.name + " age = " + student.age + " height = " + student.height + " Средний балл = " + student.SrBall());
+            Console.WriteLine("имя = {0}, рост = {1}, возраст = {2}, ср. балл = {3}",student.name,student.height,student.age,student.SrBall());
+            Console.WriteLine($"имя = {student.name}, рост = {student.height}, возраст = {student.age}, ср. балл = {student.SrBall()}");
         }
+        
     }
 }
---
+++ Students.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_02
+{
+    class Students
+    {
+        public string name;
+        public int age, height;
+        public int ballHistory, ballMatematics, ballLanguage;
+        
+        public double SrBall()
+        {
+            return ((ballHistory + ballLanguage + ballMatematics) / 3);
+        }
+
+      
+    }
+}
