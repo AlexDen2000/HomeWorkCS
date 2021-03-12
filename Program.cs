a4abbae11cd4a3ff4e9b823a0b7f6c7b70cb4c78
Alex <ADenisov2000@yandex.ru>
2021-03-12 18:08:52 +0500

    added 3HW

--- Program.cs
+++ Program.cs
@@ -1,22 +1,12 @@
 ﻿using System;
 
-namespace HomeWork_02
+namespace HomeWorkCS
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var student = new Students();
-            student.name = "Alexander";
-            student.height = 180;
-            student.age = 18;
-            student.ballHistory = 70;
-            student.ballLanguage = 68;
-            student.ballMatematics = 59;
-            Console.WriteLine("name = " + student.name + " age = " + student.age + " height = " + student.height + " Средний балл = " + student.SrBall());
-            Console.WriteLine("имя = {0}, рост = {1}, возраст = {2}, ср. балл = {3}",student.name,student.height,student.age,student.SrBall());
-            Console.WriteLine($"имя = {student.name}, рост = {student.height}, возраст = {student.age}, ср. балл = {student.SrBall()}");
+            Console.WriteLine("Hello World!");
         }
-        
     }
 }
