0756c9a87cee15ddedfd45c05e843d130c74bc14
Alex <ADenisov2000@yandex.ru>
2021-03-18 16:15:19 +0500

    task 2 comlit

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -6,7 +6,38 @@ namespace task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Сколько хотите строк теругольника паскаля: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            #region создание массива
+            int[][] array = new int[n][];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new int[i + 1];
+                array[i][0] = 1;
+                array[i][array[i].Length - 1] = 1;
+            }
+            for (int i = 2; i < array.Length; i++)
+            {
+                for (int j = 1; j < array[i].Length-1; j++)
+                {
+                    array[i][j] = array[i - 1][j - 1] + array[i-1][j];
+                }
+            }
+            #endregion
+
+
+            #region Вывод на экран массива
+            Console.WriteLine("\nВаш треугольник паскаля: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write(array[i][j]+"\t");
+                }
+                Console.WriteLine();
+            }
+            #endregion
         }
     }
 }
