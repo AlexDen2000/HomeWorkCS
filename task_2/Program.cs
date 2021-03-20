f5b452fff5dba7c40d4408315f873af8339378c2
Alex <ADenisov2000@yandex.ru>
2021-03-20 12:06:17 +0500

    task_2 done

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -6,7 +6,34 @@ namespace task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(DeletedSimilarSimvols("HHHHHHi        wwwwworllllddd"));
+        }
+        /// <summary>
+        /// Метод для удаления всех кратных рядом стоящих символов
+        /// </summary>
+        /// <param name="str">исследуемая сторока</param>
+        /// <returns></returns>
+        static string DeletedSimilarSimvols(string str)
+        {
+            string outStr=null;
+            char[] ch = new char[1];
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (ch[count]==str[i])
+                {
+                    ch[count] = str[i];
+                }
+                else
+                {
+                    Array.Resize(ref ch, ch.Length + 1);
+                    count++;
+                    ch[count] = str[i];
+                }
+            }
+            foreach (char c in ch)
+                outStr += c;
+            return outStr;
         }
     }
 }
